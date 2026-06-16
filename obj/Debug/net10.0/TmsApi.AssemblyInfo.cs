12e213b4e5fe1d78632baa6b80a48e1941eb6097
ephrem-011 <ephraimfekadu@gmail.com>
2026-06-16 08:30:40 -0700

    m5-session-1

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b04950057c6f328675751288bddbdfcfd6e2f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b89f0bb88136da5e48e4f8fd5d7b312d1c3619a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
