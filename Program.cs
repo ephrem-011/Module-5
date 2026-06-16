17b04950057c6f328675751288bddbdfcfd6e2f6
ephrem-011 <ephraimfekadu@gmail.com>
2026-06-16 06:50:29 -0700

    setup

---
+++ Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services
+    .AddAuthentication("Training")
+    .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>("Training", null);
+builder.Services.AddAuthorization();
+
+builder.Host.UseDefaultServiceProvider(options =>
+{
+    options.ValidateScopes = true;
+    options.ValidateOnBuild = true;
+});
+
+builder.Services.AddScoped<EnrollmentWorker>();
+
+builder.Services.AddSingleton<IEnrollmentService, EnrollmentService>();
+
+builder.Services.AddOptions<PaymentOptions>()
+    .BindConfiguration("Payments")
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    // Development tools only
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+else
+{
+    // Production safety
+    app.UseExceptionHandler();
+}
+app.UseStatusCodePages();
+
+app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseExceptionHandler("/error");
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
